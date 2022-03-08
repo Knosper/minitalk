e9c46bac7f5b07fe593ab9e4530e22c1d9bf6051
Knosper <79541185+Knosper@users.noreply.github.com>
2022-03-08 22:53:18 +0100

    Create minitalk.h

---
+++ minitalk.h
@@ -0,0 +1,15 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <stdio.h>
+# include <sys/types.h>
+
+void		ft_putstr(char *str);
+void		ft_putnbr(int nb);
+int		ft_atoi(const char *str);
+int		ft_strlen(char *str);
+
+#endif
