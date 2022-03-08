9961009fb5ef4e7a8be7d7222bf642102b0cde9e
Knosper <79541185+Knosper@users.noreply.github.com>
2022-03-08 22:54:04 +0100

    Create server.c

---
+++ server.c
@@ -0,0 +1,35 @@
+#include "minitalk.h"
+
+void	handler_sigusr1(int signum)
+{
+	static int	i = 0;
+	static char	c = 0;
+
+	(void)signum;
+	if (signum == SIGUSR1)
+		c += 1 << i;
+	i++;
+	if (i == 8)
+	{
+		if (c == '\0')
+			write(1, "\n", 1);
+		else
+			write(1, &c, 1);
+		i = 0;
+		c = 0;
+	}
+}
+
+int	main(void)
+{
+	pid_t	pid;
+
+	pid = getpid();
+	ft_putnbr(pid);
+	write(1, "\n", 1);
+	signal(SIGUSR1, handler_sigusr1);
+	signal(SIGUSR2, handler_sigusr1);
+	while (1)
+		;
+	return (0);
+}
